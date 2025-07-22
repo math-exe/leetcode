d185a30570300707462047646b4f67b5b91b337e
math-exe <matheus.bressan20@gmail.com>
2025-07-21 22:56:02 -0300

    Adicionado comentários no código

--- 1957-Fancy-String/src/FancyString.java
+++ 1957-Fancy-String/src/FancyString.java
@@ -3,16 +3,16 @@ public class FancyString{
     public String makeFancyString(String s) {
         StringBuilder resultado = new StringBuilder();
 
-        // Aqui eu entro com a lógica do loop
+        // Aqui eu entro com a logica do loop
         for (int i = 0; i < s.length(); i++) {
             char letraAtual = s.charAt(i);
 
-            // Aqui eu verifico se posso adicionar a letra de volta na string ou nao
-            if (resultado.length() >= 2) { // Por padrao o valor de resultado precisa ser mais que dois
+            // Verifico se posso adicionar a letra de volta na string ou nao
+            if (resultado.length() >= 2) { // Por padrao o valor de resultado precisa ser maior ou igual a dois
                 char ultimo = resultado.charAt(resultado.length() - 1);
                 char penultimo = resultado.charAt(resultado.length() -2);
 
-                /// Verificamos entao se a ultima, penultima e antepenultima letras sao iguais
+                // Verificamos entao se a ultima e penultima e letras sao iguais
                 if (ultimo == letraAtual && penultimo == letraAtual) { //
                     continue;
                 }
